6a0acf0924b1876dc07ae8e168df4980a06555a9
adt315 <angela_dyan@hotmail.com>
2020-09-09 14:22:04 -0400

    color code time blocks

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+$(document).ready(function(){
 
-var currentDay = $("#currentDay");
+    var currentDay = $("#currentDay");
+
+    function getCurrentDay(){
+        currentDay = moment().format('dddd, MMMM Do');
+        $("#currentDay").text(currentDay);
+    }
+
+    getCurrentDay();
 
 
-function getCurrentDay() {
-    currentDay = moment().format('dddd, MMMM Do');
-    $("#currentDay").text(currentDay);
-}
+    function hourUpdater(){
+        var currentHour = moment().hours();
+        $(".row").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
 
-getCurrentDay ();
+    hourUpdater();
+
+
+});
+
