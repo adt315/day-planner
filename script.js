35d9a9b74bd8f724e2258fe8ec24b591e3b0fe46
adt315 <angela_dyan@hotmail.com>
2020-09-09 23:06:47 -0400

    add JSON parse

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+// Get current day and display at top of calendar
     var currentDay = $("#currentDay");
 
-    function getCurrentDay(){
+    function getCurrentDay() {
         currentDay = moment().format('dddd, MMMM Do');
         $("#currentDay").text(currentDay);
     }
 
     getCurrentDay();
+    hourUpdater();
+    showPlans();
 
-
-    function hourUpdater(){
+// Color code timeblocks based on if they are in the past, present, or future
+    function hourUpdater() {
         var currentHour = moment().hours();
-        $(".row").each(function () {
+        $(".row").each(function() {
             var blockHour = parseInt($(this).attr("id").split("-")[1]);
             if (blockHour < currentHour) {
                 $(this).addClass("past");
@@ -29,7 +32,65 @@ $(document).ready(function(){
         })
     }
 
-    hourUpdater();
+
+// Save plan(s) in local storage
+
+    var myPlans = [
+    {
+        hour: "9",
+        plan: ""
+    },
+    {
+        hour: "10",
+        plan: ""
+    },
+    {
+        hour: "11",
+        plan: ""
+    },
+    {
+        hour: "12",
+        plan: ""
+    },
+    {
+        hour: "13",
+        plan: ""
+    },
+    {
+        hour: "14",
+        plan: ""
+    },
+    {
+        hour: "15",
+        plan: ""
+    },
+    {
+        hour: "16",
+        plan: ""
+    },
+    {
+        hour: "17",
+        plan: ""
+    },
+]
+
+    function savePlans() {
+        localStorage.setItem("myPlans", JSON.stringify(myPlans));
+    }
+        savePlans();
+   
+    function showPlans() {
+    console.log(JSON.parse(localStorage.getItem("myPlans"))) 
+        // myPlans.each(function() {
+     }
+    
+
+    $(".saveBtn").on("click", function() {
+        // fridgeMagnet.text($(this).attr("data-letter"));
+
+        savePlans();
+        showPlans();
+    });
 
 
 });
